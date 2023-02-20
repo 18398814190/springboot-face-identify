b2cfc49dea2d9148fe803b16c0f22ddc03937091
吴金蓬 <1729797709@qq.com>
2023-02-20 23:05:15 +0800

    新增人脸识别接口

--- face-sdk/src/main/java/com/wjp/autoconfig/template/FaceTemplate.java
+++ face-sdk/src/main/java/com/wjp/autoconfig/template/FaceTemplate.java
@@ -1,20 +1,91 @@
 package com.wjp.autoconfig.template;
 
-import com.alibaba.fastjson.JSONObject;
 import com.wjp.autoconfig.properties.FaceProperties;
+import com.wjp.autoconfig.util.HttpUtils;
+import org.apache.commons.codec.binary.Base64;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FaceTemplate {
 
     private FaceProperties faceProperties;
 
-    public FaceTemplate(FaceProperties faceProperties){
+    public FaceTemplate(FaceProperties faceProperties) {
         this.faceProperties = faceProperties;
     }
 
-    public JSONObject detect(File file){
+    public String detect(String imageFile) throws IOException {
+        String url = "https://api-cn.faceplusplus.com/facepp/v3/detect";
+        Map<String, String> params = new HashMap<>();
+        params.put("api_key", faceProperties.getApiKey());
+        params.put("api_secret", faceProperties.getApiSecret());
+        params.put("image_base64", fileToBase64(imageFile));
+        String result = HttpUtils.doPost(url, params, null);
+        return result;
+    }
 
-        return new JSONObject();
+
+    public String detectFile(File file) throws IOException {
+        String url = "https://api-cn.faceplusplus.com/facepp/v3/detect";
+        Map<String, String> params = new HashMap<>();
+        params.put("api_key", faceProperties.getApiKey());
+        params.put("api_secret", faceProperties.getApiSecret());
+        params.put("image_base64", fileToBase64(file));
+        String result = HttpUtils.doPost(url, params, null);
+        return result;
+    }
+
+
+    /**
+     * @Description: 文件转为base64字符串。filePath：文件路径
+     * @Param: [filePath]
+     * @return: java.lang.String
+     * @Date: 2020/12/25
+     */
+    private String fileToBase64(String filePath) throws IOException {
+        File file = new File(filePath);
+        FileInputStream inputFile = null;
+        byte[] buffer = null;
+        try {
+            inputFile = new FileInputStream(file);
+            buffer = new byte[(int) file.length()];
+            inputFile.read(buffer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != inputFile) {
+                inputFile.close();
+            }
+        }
+        byte[] bs = Base64.encodeBase64(buffer);
+        return new String(bs);
+    }
+
+    /**
+     * @Description: 文件转为base64字符串。filePath：文件路径
+     * @Param: [filePath]
+     * @return: java.lang.String
+     * @Date: 2020/12/25
+     */
+    private String fileToBase64(File file) throws IOException {
+        FileInputStream inputFile = null;
+        byte[] buffer = null;
+        try {
+            inputFile = new FileInputStream(file);
+            buffer = new byte[(int) file.length()];
+            inputFile.read(buffer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != inputFile) {
+                inputFile.close();
+            }
+        }
+        byte[] bs = Base64.encodeBase64(buffer);
+        return new String(bs);
     }
 }
--- face-service/src/main/java/com/wjp/controller/FaceRecognitionController.java
+++ face-service/src/main/java/com/wjp/controller/FaceRecognitionController.java
@@ -1,10 +1,17 @@
 package com.wjp.controller;
 
+import bean.ResultVO;
+import com.alibaba.fastjson.JSONObject;
 import com.wjp.autoconfig.template.FaceTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/face")
@@ -14,9 +21,39 @@ public class FaceRecognitionController {
     private FaceTemplate faceTemplate;
 
     @GetMapping
-    public String test(){
-        faceTemplate.detect(null);
-        return "成功";
+    public ResultVO<JSONObject> test() throws IOException {
+        String detect = faceTemplate.detect("C:\\Users\\17297\\Desktop\\test.jpg");
+
+        return ResultVO.success(JSONObject.parseObject(detect));
+    }
+
+    /**
+     * 解析人脸图片，获取face_token
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("/detect")
+    public ResultVO<JSONObject> detect(MultipartFile file) throws IOException {
+        String detect = faceTemplate.detectFile(MultipartFileToFile(file));
+
+        return ResultVO.success(JSONObject.parseObject(detect));
+    }
+
+    public static File MultipartFileToFile(MultipartFile multiFile) {
+        // 获取文件名
+        String fileName = multiFile.getOriginalFilename();
+        // 获取文件后缀
+        String prefix = fileName.substring(fileName.lastIndexOf("."));
+        // 若需要防止生成的临时文件重复,可以在文件名后添加随机码
+
+        try {
+            File file = File.createTempFile(fileName, prefix);
+            multiFile.transferTo(file);
+            return file;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
